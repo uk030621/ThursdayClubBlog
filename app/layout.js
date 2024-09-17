885b137fc7fe2981cea448f026b0d0780ff2ca86
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-09-17 12:03:54 +0100

    Added team image

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 // app/layout.js (replaces _app.js in the App Router)
 import '../styles/globals.css';
 import React from 'react';
+import Image from 'next/image';
 
 export const metadata = {
   title: 'My Next.js App',
@@ -15,9 +16,23 @@ export default function RootLayout({ children }) {
         <nav style={{paddingBottom:'20px', paddingTop:'20px'}}>
           {/* Global navigation or other components */}
           <ul>
-            <div>
-            <li style={{color:'white', marginLeft:'8px', marginBottom:'15px', fontFamily:'arial', fontWeight:'bolder', fontSize:'1.2rem'}}>Bobby and Ken&apos;s Thursday Club</li>
+            <div style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+            <li style={{color:'white', marginLeft:'0px', marginBottom:'15px', fontFamily:'arial', fontWeight:'bolder', fontSize:'1.2rem', marginRight:'0px'}}>Bobby and Ken&apos;s Thursday Club</li>
             </div>
+            <details style={{ textAlign: 'center', marginBottom: '10px' }}>
+  <summary style={{color:'yellow', cursor: 'pointer', fontSize: '1.2rem', fontFamily:'arial', fontSize:'0.7rem'}}></summary>
+  <div style={{ marginLeft: '0px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <Image
+      className='uk-pic'
+      src="/team.png"
+      alt="Portfolio Image"
+      width={300}
+      height={150}
+      display='priority'
+      style={{ marginLeft: '0px', marginBottom: '10px', borderRadius: '8px' }}
+    />
+  </div>
+</details>
             <li style={{fontFamily:'arial', marginLeft:'10px'}}><a href="/">Home</a></li>
             <li style={{fontFamily:'arial', marginLeft:'30px'}} ><a href="/create">Create Post</a></li>
           </ul>
